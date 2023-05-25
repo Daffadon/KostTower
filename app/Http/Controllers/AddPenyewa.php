1930133a4f3fb00b1bc69b33cf7f457ab139325b
BlueFZ <muhammadsyafi@student.ub.ac.id>
2023-05-25 20:35:42 +0700

    change detail route

---
+++ app/Http/Controllers/AddPenyewa.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Penyewa;
+use Illuminate\Http\Request;
+
+class AddPenyewa extends Controller
+{
+    function addPenyewa(Request $req) {
+        $model= new Penyewa;
+        $model->nik= $req->NIK;
+        $model->nama= $req->nama_lengkap;
+        $model->ttl= $req->ttl;
+        $model->agama= $req->agama;
+        $model->status= $req->status;
+        $model->kewarganegaraan= $req->kewarganegaraan;
+        $model->pekerjaan= $req->pekerjaan;
+        $model->telepon= $req->no_telp;
+        $model->address= $req->alamat;
+        $model->timestamps= false;
+        $model->save();
+        return redirect('/new-penyewa');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AddKamarController;
+use App\Http\Controllers\AddPenyewa;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PenyewaController;
 use Illuminate\Support\Facades\Route;
@@ -34,4 +36,7 @@ Route::get('/new-sewa', function () {
 Route::get('/new-penyewa', function () {
     return view('addPenyewa.index');
 });
-Route::get('/det', [PenyewaController::class, 'showPenyewa']);
+
+Route::post('/new-penyewa', [AddPenyewa::class, 'addPenyewa']);
+
+Route::get('/detail', [PenyewaController::class, 'showPenyewa']);
